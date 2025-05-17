34bb9f6496d17c966ce602269427f915b1067e91
jamesyab0i29 <jameslaffey29022004@gmail.com>
2025-05-17 15:24:45 +0100

    Add files via upload

---
+++ out/build/x64-Debug/launch-Computer_Graphics_Coursework.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/James/source/repos/Computer-Graphics-Coursework/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\James\source\repos\Computer-Graphics-Coursework\out\build\x64-Debug\Computer_Graphics_Coursework.exe"  
+pause
